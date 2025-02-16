c7a355dc16a5170223b6a9b5f92451b0e2062751
Your Name <your-email@example.com>
2025-02-16 18:56:26 +0300

    added skiils

--- myportfolio/portfolio/urls.py
+++ myportfolio/portfolio/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name='home'),
     path('portfolio/',views.portfolio,name='portfolio'),  # add a new path for portfolio page  # 'portfolio' is the name of the URL pattern, 'views.portfolio' is the corresponding view function.   # 'name' is a unique identifier for this URL pattern, it's used in templates and URL reverse function.   # 'path' is a function provided by Django's URLconf module that takes a URL pattern and a corresponding view function, and returns a URL
-   
+   path('about/',views.about,name='about'),
+   path('resume/',views.resume,name='resume'),
+   path('contact/',views.contact,name='contact'),
+
 ]
--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -8,3 +8,14 @@ def index(request):
 
 def portfolio(request):
     return render(request,'portfolio.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def resume(request):
+    return render(request,'resume.html')
+def skills(request):
+    return render(request,'skills.html')
